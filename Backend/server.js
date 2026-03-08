e7507d51bce4a8d96a25bbff63552fa868940546
AnkushSaha01 <ankush05cse@gmail.com>
2026-03-09 00:47:08 +0530

    update port

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,8 @@ const connectDB = require("./src/db/db");
 
 connectDB();
 
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server is running on port 3000`);
 });
